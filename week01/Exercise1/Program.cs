b95cdac7fa0705ab735e1d15c4b9a282f7e4e139
johnosking <johnspecial080@gmail.com>
2025-01-13 13:37:31 +0100

    Completed exercise 1 and 2 in week01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.Write("What is your first name? ");
+       Console.Write("What is your First name? ");
        string first_name = Console.ReadLine();
 
 
-       Console.Write("What is your last name? ");
+       Console.Write("What is your Last name? ");
        string last_name = Console.ReadLine();
 
 
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+     
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }    
+        else if (grade < 60)
+        {
+            letter = "F";
+        }    
+        
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Passed");
+        }    
+        else
+        {
+            Console.WriteLine("Failed");
+        }
     }
+
 }
+
