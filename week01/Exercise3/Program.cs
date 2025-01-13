9ccdebf92c22f45a0b9db4a33b494f88b0cce3b0
johnosking <johnspecial080@gmail.com>
2025-01-13 21:34:02 +0100

    Completed exercise 3 in week01 folder

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random radomGenerator = new Random();
+        int magicNumber = radomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        //Console.WriteLine("What is the magic number?. ");
+        //string answer = Console.ReadLine();
+
+        //Console.WriteLine("Guess a number between 1 and 101 ");
+        //string guess = Console.ReadLine();
+        //int magicNumber = int.Parse(answer);
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess a number between 1 and 101 ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }    
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("You did not guess right. Guess Higher");
+            }    
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("You did not guess right. Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it correctly");
+            }
+        }        
+        
     }
 }
