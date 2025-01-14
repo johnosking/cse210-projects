06100a6b9398a76fc749039b88e4729ed8ac6484
johnosking <johnspecial080@gmail.com>
2025-01-14 12:40:32 +0100

    Completed exercise 4 in week01 folder

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbersList = new List<int>();
+
+        int userList = -1;
+
+        while (userList != 0)
+        {
+            Console.WriteLine("Enter a number (O to end)");
+            string answer = Console.ReadLine();
+            userList  =  int.Parse(answer);
+            numbersList.Add(userList);
+
+        }
+
+        int sum = 0;
+        foreach (int number in  numbersList)
+        {
+            sum += number;
+
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        int average = sum / numbersList.Count;
+        Console.WriteLine($"Thee Average is {average}");
+
+    
+        int max = numbersList[0];
+
+        foreach (int number in numbersList)
+        {
+            if (number > max)
+            {
+                max = number;
+
+            }
+        }
+        Console.WriteLine($"The Max number is {max}");
     }
 }
