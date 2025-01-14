9453450987c45ec8aa956ee6a89fd7ef3d952858
johnosking <johnspecial080@gmail.com>
2025-01-14 13:49:40 +0100

    Completed exercise 5 in week01 folder

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayMessage();
+        string userName = DisplayName();
+        int userNumber = AddNumber();
+        int square = SquareNumber(userNumber);
+        DisplayResult(userName, square);
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static string DisplayName()
+    {
+        
+        Console.WriteLine($"Input your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int AddNumber ()
+    {
+        Console.WriteLine("Please enter your favorite number ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber (int userNumber)
+    {
+        int square = userNumber * userNumber;
+        return square;
+    }
+    static void DisplayResult (string userName, int square)
+    {
+        Console.WriteLine($"Your user name and squared number is {userName} and {square}");
     }
 }
